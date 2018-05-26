0f4d5fda52459edd7aa24577655132390dafc20a
jkojro <janusz.kojro@gmail.com>
2018-05-26 13:12:27 +0200

    Atom Feeds

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -64,6 +64,16 @@ class ProductsController < ApplicationController
     end
   end
 
+  def who_bought
+    @product = Product.find(parmas[:id])
+    @last_order = @products.orders.order(:updated_at).last_order
+    if stale?(@latest_order)
+      respodn_to do |format|
+        format.atom
+      end
+    end
+  end
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_product
