a5bc8705afef49964215eb32027aa15414cc9243
jkojro <janusz.kojro@gmail.com>
2018-05-31 11:10:59 +0200

    Add Session, Admin controllers and authorization

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,7 @@
 class LineItemsController < ApplicationController
 
   include CurrentCart #moduł ../controllers/concerns/current_cart.rb
+  skip_before_action :authorize, only: :create
   before_action :set_cart, only: [:create]
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
