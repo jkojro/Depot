dd42f4f391a505e8352d382bab5589161aedccc8
jkojro <janusz.kojro@gmail.com>
2019-03-16 16:32:57 +0100

    Allow products without image

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,7 @@ class Product < ApplicationRecord
 
 	before_destroy :ensure_not_referenced_by_any_line_item
 
-	validates :title, :description, :image_url, presence: true 
+	validates :title, :description, presence: true 
 	validates :price, numericality: {greater_than_or_equal_to: 0.01} 
 	validates :title, uniqueness: true, length: { minimum: 3}
 	validates :image_url, allow_blank: true, format: {
